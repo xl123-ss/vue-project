d9a281a7a38582cc70d165eeb52dbb15c3ec7296
xl123-ss <3179716692@qq.com>
2023-10-27 11:10:45 +0800

    第二版本

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="2.0版本测试" />
+      <HelloWorld msg="2.0版本测试-薛龙" />
     </div>
   </header>
 
